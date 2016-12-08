da6bc193b5ca59ad611db1a9669ce8fd16b3d777
jarlvs <premshinde13@gmail.com>
2016-12-08 19:29:59 +0530

    debug this python code

---
+++ python2.py
@@ -0,0 +1,12 @@
+# Follow along with the video to fix the bugs in this program.
+
+import random
+
+user = raw_input("Choose your weapon!")
+comp = random.choice( ['rock','paper','scissors") ]
+print 'user (you) chose:', 'user'
+print 'comp (me!) chose:', comp
+
+if User == rock and comp = 'paper'
+  print 'The result is, YOU LOSE!'
+  print 'unless you're awesome, then YOU WIN!'
