0bedbf8ca4eddb14470880153af6a646af560484
jarlvs <premshinde13@gmail.com>
2016-12-09 13:18:40 +0530

    python note 1

---
+++ note1.py
@@ -0,0 +1,59 @@
+n=13
+str(n)
+repr(n)
+`n`
+do the same thing convert number into string.
+
+import math 
+floor(44)
+sqrt(36)
+
+
+raw_input()
+get info from user and convert it to a string
+
+array=list in python 
+indexing start from 0;
+last element also have -1 index apart from regular one.
+second last has index -2 so and so forth..
+
+strings are list
+'prem'
+p is 0,r is 1 like that.
+
+slicing 
+l=[1,2,3,4,2,4,2,5]
+l[3:6]=[4,2,4]  start with index 3 
+end with index 5 one less than 6.
+
+l[3:] from index 3 till end [4,2,4,2,5]
+l[:7] from start till index less than 7 
+that is 6  [1,2,3,4,2,4,2]
+
+editing sequence 
+[7,5,4]+[5,6,7]=[7,5,4,5,6,7]
+[4,3,5]+'prem' will give error
+
+'prem'*4='premprempremprem'
+[4,3]*3=[4,3,4,3,4,3]
+4*3=12
+
+name='prem'
+'z' in name will give false as there is no 'z' 
+in 'prem'.
+
+family=['mom','dad','bro']
+'sis' in family
+false
+lie that
+
+len(parameter) will give length of parameter,
+parameter can be list ,sequence
+
+max(parameter) will give max element in list
+same for min.
+list('prem') will convert to ['p','r','e','m'] 
+
+del number[3] will delect the number at index 3.
+
+look at slicing once.
