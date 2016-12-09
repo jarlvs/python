d58915ea4e85dfd3e4cc6a950a313ef825bcabfb
jarlvs <premshinde13@gmail.com>
2016-12-09 18:21:38 +0530

    python note 2

---
+++ note2.py
@@ -0,0 +1,56 @@
+array.append(45)
+it will add 45 to the array as a last element.
+
+array.count('apples')
+will give how many times 'apples' appear in array
+
+one.extend(two)  if one and two are array then 
+array two get added to array one.
+
+array.index('apples')
+will give index of 'apples' in array
+
+array.insert(2,'hoss')
+will insert 'hoss' at index 2
+
+array.pop(1) will return element you delected.
+and also delect the element in array
+
+array.remove('hoss') will delect the 'hoss' from
+array but not return like pop() method
+
+array.reverse() will reverse the array
+
+sorted('Prem')
+['P','e','m','r']
+ 
+a=[4,36,72,2,1]
+a.sort() 
+[1,2,4,36,72] like that
+
+Tuples 
+only gives our values back using index that it.
+n=(1,24,5)
+n[2] will give 5 back
+
+k='prem'
+k.upper()
+'PREM'
+same for lower()
+
+truth='I love you'
+truth.replace('you'.'myself')
+truth will now become 'I love myself'
+
+truth='%s love %s'
+ver=('I','myself')
+print truth %  ver
+will print I Love myself
+
+truth.find('Love')
+will give index where Love word start
+here it is 2
+
+
+
+
