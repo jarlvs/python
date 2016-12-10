d998e2ebfef0a28a4930be25cbd6bc2f5efef967
jarlvs <premshinde13@gmail.com>
2016-12-11 01:39:49 +0530

    python note 3

---
+++ note3.py
@@ -0,0 +1,45 @@
+dictionary 
+a={'dad':'42','mom':'12','bro':'25'}
+a.clear()
+b=a.copy()
+b is same as a
+a.has_key('key')
+will give True if 'key' is in your dictionary.
+
+p="Pizzahut"
+'s' in p
+will return false as 's' is not in p.
+
+a=9
+if a>5 and a<20:
+	body
+use of 'and' same for 'or' also
+
+while a<=20:
+	print a
+	a+=1
+
+gl=['bread','milk','beef','meet']
+for food in gl:
+	print 'I want'+ food
+
+for dictionary a(at the top)
+for item in a:
+	print a
+will print all the keys
+
+for item in a:
+	print a[item]
+will print value corresponding to each key
+
+
+break statement 
+while 1:
+	name=raw_input('Enter name: ')
+	if name=='quit':break
+
+function 
+def whatsapp(x):
+	return 'whatsapp' +x
+
+
