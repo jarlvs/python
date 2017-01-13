fe03580fc57bfb9a15ec8994626bbceb8760f4f9
jarlvs <premshinde13@gmail.com>
2017-01-13 16:51:25 +0530

    note4 eddited

--- note4.py
+++ note4.py
@@ -1,1 +1,16 @@
-hii
+phonebook = {}
+phonebook["John"] = 938477566
+phonebook["Jack"] = 938377264
+phonebook["Jill"] = 947662781
+
+for name, number in phonebook.items():
+	print name
+	print number
+will 'print' out  first key and then there number for all 
+keys.
+
+for name in phonebook.items():
+	print name
+'print' all key vaue pair.
+
+del phonebook["John"] wil delect key-value pair of John.
